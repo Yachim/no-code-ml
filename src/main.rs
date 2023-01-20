82768297b8f8b811b24b4e64cddd43c57a8ed334
Yachim <jachym.kohout@gmail.com>
2023-01-20 15:51:01 +0100

    Add relu function and its derivative

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use utils::functions;
 fn main() {
     println!("{}", functions::sigmoid(1.0));
     println!("{}", functions::sigmoid_deriv(1.0));
+
+    println!("{}", functions::relu(42.0));
+    println!("{}", functions::relu_deriv(42.0));
+    println!("{}", functions::relu(-42.0));
+    println!("{}", functions::relu_deriv(-42.0));
 }
--- src/utils/functions.rs
+++ src/utils/functions.rs
@@ -7,3 +7,21 @@ pub fn sigmoid(n: f32) -> f32 {
 pub fn sigmoid_deriv(n: f32) -> f32 {
     return sigmoid(n)*(1.0 - sigmoid(n));
 }
+
+pub fn relu(n: f32) -> f32 {
+    if n > 0.0 {
+        return n;
+    }
+    else {
+        return 0.0;
+    }
+}
+
+pub fn relu_deriv(n: f32) -> f32 {
+    if n > 0.0 {
+        return 1.0;
+    }
+    else {
+        return 0.0;
+    }
+}
