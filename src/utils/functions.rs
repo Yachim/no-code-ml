fb58e80adb2f19aac57254103ecda9351c18ed43
Yachim <jachym.kohout@gmail.com>
2023-01-20 18:38:58 +0100

    Add documentation on functions

--- src/utils/functions.rs
+++ src/utils/functions.rs
@@ -1,13 +1,18 @@
 use std::f32::consts;
 
+/// Sigmoid activation function
+/// https://en.wikipedia.org/wiki/Sigmoid_function
 pub fn sigmoid(n: f32) -> f32 {
     return 1.0 / (1.0 + consts::E.powf(-n));
 }
 
+/// Derivative of the sigmoid activation function
 pub fn sigmoid_deriv(n: f32) -> f32 {
     return sigmoid(n) * (1.0 - sigmoid(n));
 }
 
+/// ReLU activation function
+/// https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
 pub fn relu(n: f32) -> f32 {
     if n > 0.0 {
         return n;
@@ -16,6 +21,7 @@ pub fn relu(n: f32) -> f32 {
     }
 }
 
+/// Derivative of the ReLU activation function
 pub fn relu_deriv(n: f32) -> f32 {
     if n > 0.0 {
         return 1.0;
