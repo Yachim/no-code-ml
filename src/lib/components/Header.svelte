6f69f27030b4c06bc230504e8d098d530dcda18f
Yachim <jachym.kohout@gmail.com>
2023-03-11 16:48:07 +0100

    Add edit button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Fa from "svelte-fa";
-	import { faBars } from "@fortawesome/free-solid-svg-icons";
+	import { faBars, faPencil } from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <header class="w-full px-4 py-2 gap-4 text-2xl bg-headerBg flex">
@@ -8,7 +8,12 @@
 		<Fa icon={faBars} />
 	</button>
 	<div>
-		<h1 class="text-lg">Unnamed network</h1>
+		<h1 class="text-lg flex gap-2">
+			Unnamed network
+			<button class="ignore" title="Edit network">
+				<Fa icon={faPencil} />
+			</button>
+		</h1>
 		<h2 class="text-base">Multilayer Perceptron</h2>
 	</div>
 </header>
