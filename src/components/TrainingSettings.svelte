6c740e705aa6111b6fd575d62fa6e07f31aff97d
Yachim <jachym.kohout@gmail.com>
2023-03-11 09:49:40 +0100

    Add input to TrainingSettings

--- src/components/TrainingSettings.svelte
+++ src/components/TrainingSettings.svelte
@@ -1,5 +1,22 @@
+<script lang="ts">
+	import Fa from "svelte-fa";
+	import { faPlay } from "@fortawesome/free-solid-svg-icons";
+
+	let iteration_cnt = 10000;
+</script>
+
 <div class="absolute top-3 right-3 text-right">
-	<p>Iterations: <button class="underline cursor-pointer">10000</button></p>
+	<p>
+		Iterations: <input
+			type="number"
+			class="bg-bg border-2 border-gray-700 px-2 box-content rounded-lg underline cursor-pointer"
+			style="width: {String(iteration_cnt).length + 2}ch"
+			bind:value={iteration_cnt}
+		/>
+		<button>
+			<Fa icon={faPlay} />
+		</button>
+	</p>
 	<p>
 		Cost (<button class="underline cursor-pointer">MSE</button>): 0.0
 	</p>
