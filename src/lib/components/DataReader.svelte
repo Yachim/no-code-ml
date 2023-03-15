199b275ca27679479f272005eb912ba758785599
Yachim <jachym.kohout@gmail.com>
2023-03-15 13:53:19 +0100

    WIP filtering datareader

--- src/lib/components/DataReader.svelte
+++ src/lib/components/DataReader.svelte
@@ -82,26 +82,85 @@
 	}
 
 	const maxCols = 10;
+
+	let labelCol: string;
+	let filteredHeaders: string[];
+	let filteredData: string[][];
+
+	$: if (files) {
+		let labelIndex = +labelCol;
+
+		// if labelIndex is undefined - filteredHeaders has first header and i = 1
+		filteredHeaders = labelIndex ? [headers[labelIndex]] : [headers[0]];
+		let i = labelIndex ?? false ? 0 : 1;
+
+		let accumulated = 0;
+
+		while (accumulated < maxCols - 1) {
+			if (i === labelIndex) {
+				i++;
+				continue;
+			}
+
+			filteredHeaders.push(headers[i]);
+			i++;
+			accumulated++;
+		}
+
+		filteredData = data.map((line) => {
+			// if labelIndex is undefined - filteredHeaders has first header and i = 1
+			const filteredLine =
+				labelIndex ?? false ? [line[labelIndex]] : [line[0]];
+			let i = labelIndex ? 0 : 1;
+
+			let accumulated = 0;
+
+			while (accumulated < maxCols - 1) {
+				if (i === labelIndex) {
+					i++;
+					continue;
+				}
+
+				filteredLine.push(line[i]);
+				i++;
+				accumulated++;
+			}
+
+			return filteredLine;
+		});
+	}
 </script>
 
 <div class={!files ? "grid place-items-center h-full" : "flex flex-col gap-4"}>
-	<input type="file" accept=".csv" class="ignore" bind:files />
-	{#if files}
+	<div class={!files ? "" : "flex justify-between"}>
+		<input type="file" accept=".csv" class="ignore" bind:files />
+
+		{#if files}
+			<label>
+				Label colum:
+				<input bind:value={labelCol} list="header-list" />
+
+				<datalist id="header-list">
+					{#each headers as header, i}
+						<option value={i}>{header}</option>
+					{/each}
+				</datalist></label
+			>
+		{/if}
+	</div>
+
+	{#if files && filteredHeaders && filteredData}
 		<table class="w-full border">
 			<tr class="bg-border">
-				{#if headers}
-					{#each headers.slice(0, maxCols) as cell}
-						<th class="text-center border py-2">{cell}</th>
-					{/each}
-				{/if}
+				{#each filteredHeaders as cell}
+					<th class="text-center border py-2">{cell}</th>
+				{/each}
 			</tr>
-			{#each data as line, i}
+			{#each filteredData as line, i}
 				<tr class:bg-headerBg={i % 2} class:bg-opacity-50={i % 2}>
-					{#if line}
-						{#each line.slice(0, maxCols) as cell}
-							<td class="text-center border py-2">{cell}</td>
-						{/each}
-					{/if}
+					{#each line.slice(0, maxCols) as cell}
+						<td class="text-center border py-2">{cell}</td>
+					{/each}
 				</tr>
 			{/each}
 		</table>
