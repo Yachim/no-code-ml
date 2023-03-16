91bc52e431b37815c0f427dbe997838db1cc6cd9
Yachim <jachym.kohout@gmail.com>
2023-03-16 19:25:03 +0100

    Add mutation to create net

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,17 +1,37 @@
-import { useQuery } from "@sveltestack/svelte-query";
+import { useMutation, useQuery, useQueryClient } from "@sveltestack/svelte-query";
 import type { NetList } from "../types/savedData";
 
 import { exists, readTextFile, BaseDirectory } from "@tauri-apps/api/fs";
 
+// params for readTextFile/exists
+const netsFileReadSettings = ["nets.json", { dir: BaseDirectory.AppData }] as const;
+
+const queryClient = useQueryClient();
+
 // loads the data file containing list of networks
 function useNetList() {
-	return useQuery<NetList>("nets", async () => {
-		const fileSettings = ["nets.json", { dir: BaseDirectory.AppData }] as const;
-
-		if (await exists(...fileSettings)) {
-			const file = await readTextFile(...fileSettings)
-			return JSON.parse(file)
+	return useQuery<NetList>("netList", async () => {
+		if (await exists(...netsFileReadSettings)) {
+			const file = await readTextFile(...netsFileReadSettings);
+			return JSON.parse(file);
 		}
-		return []
+		return [];
+	})
+}
+
+function useCreateNet() {
+	return useMutation(async (name: string) => {
+		let data: NetList = (await exists(...netsFileReadSettings)) ?
+			JSON.parse(await readTextFile(...netsFileReadSettings)) :
+			[];
+
+		data = [...data, {
+			name,
+			id: crypto.randomUUID()
+		}];
+
+		return data;
+	}, {
+		onSuccess: () => queryClient.invalidateQueries("netList")
 	})
 }
